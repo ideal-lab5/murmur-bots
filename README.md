a68ddafc5755dd8677da4bdb94911f10278101be
Juan Girini <juangirini@gmail.com>
2024-10-18 13:57:04 +0200

    chore: license and styling

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Murmur Bots
 
-This is a collection of node.js-based implementations of the murmur protocol. Specifically, this repo contains bot integrations for both [Discord](https://discordapp.com/) and [Twitch](https://www.twitch.tv/), enabling per-channel/server wallets via Murmur.
+This is a collection of node.js-based implementations of the [Murmur protocol](https://github.com/ideal-lab5/murmur). Specifically, this repo contains bot integrations for both [Discord](https://discordapp.com/) and [Twitch](https://www.twitch.tv/), enabling per-channel/server wallets via Murmur.
 
 To learn more about the Murmur protocol, visit the documentation at https://murmur.idealabs.network.
 
@@ -10,23 +10,24 @@ To learn more about the Murmur protocol, visit the documentation at https://murm
 
 #### Setup
 
-First create a file called .env.discord in the root of this repo containing the following keys:
+First create a file called `.env.discord` in the root of this repo containing the following keys:
 
-``` 
+```
 MAX_RETRIES=number of times to retry failed transactions
 SECRET_SALT=any string
 DISCORD_BOT_TOKEN=a valid discord bot token for the server
 ```
 
-Once configured, install dependencies and run the bot with
-``` shell
+Once configured, install dependencies and run the Discord bot with
+
+```shell
 npm i
 npm run discord
 ```
 
 ### Twitch
 
-Create a file called .env.twitch in the root of this repo containing the following keys:
+Create a file called `.env.twitch` in the root of this repo containing the following keys:
 
 ```
 MAX_RETRIES=
@@ -36,9 +37,21 @@ TWITCH_OAUTH_TOKEN=<twitch chat oauth token>
 TWITCH_CHANNEL=<twitch channel name (e.g. your username)>
 ```
 
-Then instsall depenencies and run the twitch bot with
+Then instsall depenencies and run the Twitch bot with
 
-``` shell
+```shell
 npm i
 npm run twitch
 ```
+
+## Contributing
+
+Contributions are welcome! Please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the Apache-2.0. See the [LICENSE](LICENSE) file for details.
+
+## Contact
+
+For any inquiries, please contact [Ideal Labs](https://idealabs.network).
--- src/twitch.js
+++ src/twitch.js
@@ -17,18 +17,23 @@
 import tmi from 'tmi.js'
 import { BN } from 'bn.js'
 import dotenv from 'dotenv'
-import { MurmurService, generateSecret, isAuthenticated, executeWithRetries } from './index.js'
+import {
+  MurmurService,
+  generateSecret,
+  isAuthenticated,
+  executeWithRetries,
+} from './index.js'
 
 dotenv.config({ path: '.env.twitch' })
 const TWITCH = 'twitch'
 
 // Twitch bot configuration
 const client = new tmi.Client({
-    identity: {
-        username: process.env.TWITCH_BOT_USERNAME,
-        password: process.env.TWITCH_OAUTH_TOKEN,
-    },
-    channels: [process.env.TWITCH_CHANNEL]
+  identity: {
+    username: process.env.TWITCH_BOT_USERNAME,
+    password: process.env.TWITCH_OAUTH_TOKEN,
+  },
+  channels: [process.env.TWITCH_CHANNEL],
 })
 
 // Store user sessions to track authentication
@@ -37,99 +42,133 @@ const userSessions = {}
 const murmurService = new MurmurService()
 
 client.connect().then(() => {
-    console.log('Twitch Bot is online!')
+  console.log('Twitch Bot is online!')
 })
 
 // Twitch bot commands
 client.on('message', async (channel, tags, message, self) => {
-    if (self) return // Ignore messages from the bot
+  if (self) return // Ignore messages from the bot
 
-    const args = message.split(' ')
-    let command = args[1]
-    if (command === undefined) return
-    command = command.toLowerCase()
+  const args = message.split(' ')
+  let command = args[1]
+  if (command === undefined) return
+  command = command.toLowerCase()
 
-    // Auth command
-    if (command === '!auth') {
-        const secret = generateSecret(tags['user-id'])
-        const username = tags['display-name']
+  // Auth command
+  if (command === '!auth') {
+    const secret = generateSecret(tags['user-id'])
+    const username = tags['display-name']
 
-        try {
-            murmurService.authenticate(username + TWITCH, secret).then(() => {
-                userSessions[tags['user-id']] = { authenticated: true, secret }
-                client.say(channel, `@${username}, authenticated successfully.`)
-            })
-        } catch (err) {
-            console.log(err)
-            client.say(channel, `@${username}, authentication failed.`)
-        }
+    try {
+      murmurService.authenticate(username + TWITCH, secret).then(() => {
+        userSessions[tags['user-id']] = { authenticated: true, secret }
+        client.say(channel, `@${username}, authenticated successfully.`)
+      })
+    } catch (err) {
+      console.log(err)
+      client.say(channel, `@${username}, authentication failed.`)
+    }
+  }
+
+  if (command === '!drip') {
+    if (!isAuthenticated(tags['user-id'], userSessions)) {
+      return client.say(
+        channel,
+        `@${tags['display-name']}, please authenticate using \`!auth\` first.`
+      )
     }
 
-    if (command === '!drip') {
-        if (!isAuthenticated(tags['user-id'], userSessions)) {
-            return client.say(channel, `@${tags['display-name']}, please authenticate using \`!auth\` first.`)
-        }
+    const username = tags['display-name']
+    const userInfo = await murmurService.inspectUser(username + TWITCH)
+    if (userInfo.address == '')
+      return client.say(channel, 'You must call !create <validity> first.')
+    const address = userInfo.address
+    murmurService.faucet(address, () => {
+      client.say(channel, `Sent 500 tokens to @${username}.`)
+    })
+  }
 
-        const username = tags['display-name']
-        const userInfo = await murmurService.inspectUser(username + TWITCH)
-        if (userInfo.address == '') return client.say(channel, 'You must call !create <validity> first.')
-        const address = userInfo.address;
-        murmurService.faucet(address, () => {
-            client.say(channel, `Sent 500 tokens to @${username}.`)
-        });
-
+  // Create command (requires authentication)
+  if (command === '!create') {
+    if (!isAuthenticated(tags['user-id'], userSessions)) {
+      return client.say(
+        channel,
+        `@${tags['display-name']}, please authenticate using \`!auth\` first.`
+      )
     }
 
-    // Create command (requires authentication)
-    if (command === '!create') {
-        if (!isAuthenticated(tags['user-id'], userSessions)) {
-            return client.say(channel, `@${tags['display-name']}, please authenticate using \`!auth\` first.`)
-        }
+    const validity = parseInt(args[2]) || 10 // Set default validity
+    murmurService.createNew(validity, (result) => {
+      if (result.status.isInBlock)
+        client.say(
+          channel,
+          `@${tags['display-name']}, created new entry with validity: ${validity}.`
+        )
+    })
+  }
 
-        const validity = parseInt(args[2]) || 10 // Set default validity
-        murmurService.createNew(validity, (result) => {
-            if (result.status.isInBlock)
-                client.say(channel, `@${tags['display-name']}, created new entry with validity: ${validity}.`)
-        })
+  // Execute command (requires authentication)
+  if (command === '!execute') {
+    if (!isAuthenticated(tags['user-id'], userSessions)) {
+      return client.say(
+        channel,
+        `@${tags['display-name']}, please authenticate using \`!auth\` first.`
+      )
     }
 
-    // Execute command (requires authentication)
-    if (command === '!execute') {
-        if (!isAuthenticated(tags['user-id'], userSessions)) {
-            return client.say(channel, `@${tags['display-name']}, please authenticate using \`!auth\` first.`)
-        }
-
-        const username = tags['display-name']
-        const userInfo = await murmurService.inspectUser(username + TWITCH)
-        if (userInfo === '') {
-            return client.say(channel, `@${username}, you must first create a wallet with !create.`)
-        }
-
-        const recipient = args[2]
-        const amount = args[3]
-
-        let balance = new BN(amount * Math.pow(10, 12))
-        let tx = await murmurService.api.tx.balances.transferKeepAlive(recipient, balance)
-
-        executeWithRetries(murmurService, tx, 
-            () => {
-            client.say(channel, `@${username}, transaction executed successfully.`);
-        }, (retries) => {
-            client.say(channel, `@${username}, transaction execution failed (timing). Attempting retry ${retries + 1} of ${process.env.MAX_RETRIES}`)
-        }, () => {
-            client.say(channel, `@${username}, maximum retry limit reached. Transaction failed.`)
-        })
+    const username = tags['display-name']
+    const userInfo = await murmurService.inspectUser(username + TWITCH)
+    if (userInfo === '') {
+      return client.say(
+        channel,
+        `@${username}, you must first create a wallet with !create.`
+      )
     }
 
-    // Inspect command (publicly accessible)
-    if (command === '!inspect') {
-        const username = tags['display-name']
-        const userInfo = await murmurService.inspectUser(username + TWITCH)
-        client.say(channel, `User ${username} has address: ${userInfo.address} and balance: ${userInfo.balance}`)
-    }
+    const recipient = args[2]
+    const amount = args[3]
 
-    if (command === '!help') {
-        client.say(channel, `
+    let balance = new BN(amount * Math.pow(10, 12))
+    let tx = await murmurService.api.tx.balances.transferKeepAlive(
+      recipient,
+      balance
+    )
+
+    executeWithRetries(
+      murmurService,
+      tx,
+      () => {
+        client.say(channel, `@${username}, transaction executed successfully.`)
+      },
+      (retries) => {
+        client.say(
+          channel,
+          `@${username}, transaction execution failed (timing). Attempting retry ${retries + 1} of ${process.env.MAX_RETRIES}`
+        )
+      },
+      () => {
+        client.say(
+          channel,
+          `@${username}, maximum retry limit reached. Transaction failed.`
+        )
+      }
+    )
+  }
+
+  // Inspect command (publicly accessible)
+  if (command === '!inspect') {
+    const username = tags['display-name']
+    const userInfo = await murmurService.inspectUser(username + TWITCH)
+    client.say(
+      channel,
+      `User ${username} has address: ${userInfo.address} and balance: ${userInfo.balance}`
+    )
+  }
+
+  if (command === '!help') {
+    client.say(
+      channel,
+      `
             Available Commands:
                 !auth - Authenticate with the service.
                 !create <validity> - Create a new entry with optional validity.
@@ -137,6 +176,7 @@ client.on('message', async (channel, tags, message, self) => {
                 !execute <recipient> <amount> - Execute a token transfer (requires authentication).
                 !inspect - Inspect your current balance and address.
                 !help - Display the help message
-        `)
-    }
+        `
+    )
+  }
 })
